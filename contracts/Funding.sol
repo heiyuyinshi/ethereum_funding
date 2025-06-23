0fe8483bc6ec9c59cc786a99a41b8898363b04a2
heiyuyinshi <936189985@qq.com>
2025-06-23 22:52:43 +0800

    add refund

--- contracts/Funding.sol
+++ contracts/Funding.sol
@@ -19,7 +19,7 @@ contract Funding {
     uint256 public endTime;
 
     // 维护所有参与者
-    address[] investors;
+    payable address[] investors;
 
     constructor(string memory _projectName, uint256 _targetMoney, uint256 _supportMoney, uint256 _duration) {
         manager = msg.sender;
@@ -29,16 +29,33 @@ contract Funding {
         endTime = block.timestamp + _duration;
     }
 
+    // 投资者进行投资
     function invest() payable public {
         require(msg.value == supportMoney);
         investors.push(msg.sender);
     }
 
+    // 查看当前总额
     function getBalance() public view returns (uint256) {
         return address(this).balance;
     }
 
+    // 获取当前投资者
     function getInvestors()public view returns (address[] memory) {
         return investors;
     }
+
+    // 退款，由外层（前端）调用
+    function refund() public {
+
+        for (uint256 i = 0; i < investors.length; i++) {
+            investors[i].transfer(supportMoney);
+        }
+        // 投资者清零
+        delete investors;
+    }
+
+    // 花费请求
+    
+
 }
